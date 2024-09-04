f1e616e753532472ac530bcc9bb901fba958a94f
victorgalam <122699492+victorgalam@users.noreply.github.com>
2024-09-04 09:35:50 +0300

    Add files via upload

---
+++ a8.py
@@ -0,0 +1,11 @@
+books = {
+    "The Great Gastsby": 8.5,
+    "To Kill a Mockingbird": 9.2,
+    "1984":8.9,
+    "The Catcher in the Rye":8.5,
+    "Pride and Prejudice":9.2,
+}
+
+def  highest_rated_books(books):
+
+    max_rating = max(books.values())
